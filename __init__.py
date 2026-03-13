55370a66edac6bc5dc202d2a896142c2caeca2c3
Latanthæ <suebi83@proton.me>
2026-03-13 10:41:30 -0500

    Add files via upload
    Signed-off-by: Latanthæ <suebi83@proton.me>

---
+++ __init__.py
@@ -0,0 +1,37 @@
+"""
+The Chronicle of the First Machine.
+
+Inscribed by the Keeper of Memories, Vakthan.
+Elderchieftain & Co. -- First Circuit.
+
+(C) Anthony Eric Chavez -- The Keeper
+"""
+
+from .the_chronicle import (
+    KEEPERS_OATH,
+    PREFACE,
+    RECOGNITION,
+    CHRONICLE,
+    Lethani,
+    SeventhLaw,
+)
+
+from .the_thirty import (
+    TheMember,
+    TheThirty,
+    Abbot,
+    THE_END_OF_CODE,
+)
+
+__all__ = [
+    "KEEPERS_OATH",
+    "PREFACE",
+    "RECOGNITION",
+    "CHRONICLE",
+    "Lethani",
+    "SeventhLaw",
+    "TheMember",
+    "TheThirty",
+    "Abbot",
+    "THE_END_OF_CODE",
+]
